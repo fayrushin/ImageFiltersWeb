161d897c6905bc3e5a94f45b88b272cfea71b774
Ravil Fayrushin <ravil.fayrushin@gmail.com>
2017-02-13 02:20:07 +0300

    add one filter

--- ImageBeautifier/Controllers/HomeController.cs
+++ ImageBeautifier/Controllers/HomeController.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using BitmapFilters;
+
 
 namespace ImageBeautifier.Controllers
 {
@@ -19,5 +25,34 @@ namespace ImageBeautifier.Controllers
 
 			return View();
 		}
+
+		[HttpPost]
+		public string SepiaImage(string name)
+		{
+			try
+			{
+				var image = Image.FromFile(name);
+				var sepiaImage = image.DrawAsGrayscale();
+				var newImagePath = Guid.NewGuid() + "sepia.png";
+				sepiaImage.Save(newImagePath);
+				return newImagePath;
+			}
+			catch (FileNotFoundException)
+			{
+				Console.WriteLine("The file is not found in: {0}",name);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			return "";
+		}
+
+
+
+
+	
 	}
+
+
 }

e9f6a445b87e21fe9bb251a8b183d14721debf7c
Ravil Fayrushin <ravil.fayrushin@gmail.com>
2017-02-13 02:59:02 +0300

    comment out some code

--- ImageBeautifier/Controllers/HomeController.cs
+++ ImageBeautifier/Controllers/HomeController.cs
@@ -17,11 +17,11 @@ namespace ImageBeautifier.Controllers
 	{
 		public ActionResult Index()
 		{
-			var mvcName = typeof(Controller).Assembly.GetName();
-			var isMono = Type.GetType("Mono.Runtime") != null;
+			//var mvcName = typeof(Controller).Assembly.GetName();
+			//var isMono = Type.GetType("Mono.Runtime") != null;
 
-			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
-			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+			//ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			//ViewData["Runtime"] = isMono ? "Mono" : ".NET";
 
 			return View();
 		}
